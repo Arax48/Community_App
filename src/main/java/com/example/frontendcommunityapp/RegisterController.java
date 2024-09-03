1a1ccc60f7dc7bda11fecb37ede3affae75d579c
Arax48 <159503056+Arax48@users.noreply.github.com>
2024-09-03 15:59:54 -0500

    Se añade ChoiceBox a pag registro y mensaje de error para registrar vigilante

--- src/main/java/com/example/frontendcommunityapp/RegisterController.java
+++ src/main/java/com/example/frontendcommunityapp/RegisterController.java
@@ -4,22 +4,26 @@ import com.example.frontendcommunityapp.Model.Users.Admin;
 import com.example.frontendcommunityapp.Model.Users.Resident;
 import com.example.frontendcommunityapp.Model.Users.User;
 import com.example.frontendcommunityapp.Model.Users.Vigilante;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RegisterController {
+public class RegisterController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -45,7 +49,7 @@ public class RegisterController {
 
     @FXML
     private TextField textFieldApto;
-
+    /*
     @FXML
     private RadioButton radioButtonAdmin;
 
@@ -55,11 +59,26 @@ public class RegisterController {
     @FXML
     private RadioButton radioButtonVigilante;
 
+    */
+
+    @FXML
+    private Label errorMessage;
+
+    @FXML
+    private ChoiceBox<String> users;
+
     @FXML
     private Button buttonRegistrar;
 
+
+    private String[] usuarios = {"Admin", "Residente", "Vigilante"};
     boolean isSelected = false;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        users.getItems().addAll(usuarios);
+    }
 
 
     public void registrarUsuario(javafx.event.ActionEvent actionEvent) throws IOException {
@@ -72,53 +91,41 @@ public class RegisterController {
         String torre = textFieldTorre.getText();
         String apto = textFieldApto.getText();
 
-        //Hay que oprimir el botón primero y luego seleccionar tipo de usuario
-        radioButtonAdmin.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                // RadioButton is now selected
-                System.out.println("RadioButton is selected.");
-                Admin bob = new Admin(12, nombre,
-                        telefono, email, pass, username, torre, apto );
+        String user = users.getValue();
+        System.out.println(user);
 
-                bob.registrarAdminDB(nombre, telefono, email, pass, username, torre, apto);
+
+
+            if (user == "Admin") {
+                Admin createdAdmin = new Admin(12, nombre,
+                        telefono, email, pass, username, torre, apto);
+
+                createdAdmin.registrarAdminDB(nombre, telefono, email, pass, username, torre, apto);
                 System.out.println("Admin creado con éxtio");
-            } else {
-                // RadioButton is now deselected
-                System.out.println("RadioButton is deselected.");
             }
-        });
 
-        radioButtonResident.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                // RadioButton is now selected
-                System.out.println("RadioButton is selected.");
-                Resident alex = new Resident(12, nombre,
+            else if (user == "Residente") {
+
+                Resident createdResident = new Resident(12, nombre,
                         telefono, email, pass, username, torre, apto );
 
-                alex.registrarResidentDB(nombre, telefono, email, pass, username, torre, apto);
+                createdResident.registrarResidentDB(nombre, telefono, email, pass, username, torre, apto);
                 System.out.println("Residente creado con éxtio");
-            } else {
-                // RadioButton is now deselected
-                System.out.println("RadioButton is deselected.");
             }
-        });
+
+            else {
+                if(!torre.isEmpty() || !apto.isEmpty() ){
+                    errorMessage.setText("Vigilante no se le asocia torre y apto favor dejar los campos vacios");
+                }
 
 
-        radioButtonVigilante.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                // RadioButton is now selected
-                System.out.println("RadioButton is selected.");
-                Vigilante pedro = new Vigilante(12, nombre,
+                Vigilante createdVigilante = new Vigilante(12, nombre,
                         telefono, email, pass, username);
 
-                pedro.registrarVigilanteDB(nombre, telefono, email, pass, username);
+                createdVigilante.registrarVigilanteDB(nombre, telefono, email, pass, username);
                 System.out.println("Vigilante creado con éxtio");
-            } else {
-                // RadioButton is now deselected
-                System.out.println("RadioButton is deselected.");
-            }
-        });
 
+            }
 
     }
 
@@ -133,5 +140,7 @@ public class RegisterController {
         stage.setScene(scene);
         stage.show();
     }
+
+
 }
 
